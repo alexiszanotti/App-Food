c572568f774da175d62784aa486bcd140af550d4
Alexis Zanotti <alexiszanotti@gmail.com>
2023-02-24 12:52:28 +0100

    Update thunkRecipes.js

--- client/src/Redux/slice/thunkRecipes.js
+++ client/src/Redux/slice/thunkRecipes.js
@@ -3,7 +3,7 @@ import recipesApi from "./../../api/recipesApi";
 
 export const fetchAllRecipes = () => async dispatch => {
   try {
-    const { data } = await recipesApi(`/recipes`);
+    const { data } = await recipesApi(`/api/recipes`);
     dispatch(setRecipes(data));
   } catch (error) {
     console.log(error);
@@ -20,7 +20,7 @@ export const orderRecipesByName = payload => async dispatch => {
 
 export const searchRecipeByName = name => async dispatch => {
   try {
-    const { data } = await recipesApi(`/recipes?name=${name}`);
+    const { data } = await recipesApi(`/api/recipes?name=${name}`);
     dispatch(searchByName(data));
   } catch (error) {
     console.log(error);
@@ -29,7 +29,7 @@ export const searchRecipeByName = name => async dispatch => {
 
 export const fetchDetails = idRecipe => async dispatch => {
   try {
-    const { data } = await recipesApi(`/recipes/${idRecipe}`);
+    const { data } = await recipesApi(`/api/recipes/${idRecipe}`);
 
     dispatch(getDetails(data));
   } catch (error) {
@@ -40,7 +40,7 @@ export const fetchDetails = idRecipe => async dispatch => {
 export const postRecipe = payload => async dispatch => {
   try {
     dispatch(postRecipes(payload));
-    await recipesApi.post("/recipe", payload);
+    await recipesApi.post("/api/recipe", payload);
   } catch (error) {
     console.log(error);
   }
